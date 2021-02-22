a974860e42b9286d0aff7c10bb259151d8353801
Gudo Fabi <godofredo.fabi@growsuper.com>
2021-02-23 01:19:55 +0800

    fix: add tailwind

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+
+import "../src/assets/index.css";
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
 
